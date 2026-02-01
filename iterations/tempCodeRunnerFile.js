1aa7a5c2297d8bc3bf0a44f5391a688581b72453
VaRiAbleX1 <ranjanayush2021@gmail.com>
2026-02-02 03:04:46 +0530

    loops

--- iterations/tempCodeRunnerFile.js
+++ iterations/tempCodeRunnerFile.js
@@ -1,29 +1,3 @@
-// const coding = ["js", "ruby", "java", "python", "cpp"]
-
-
-// const values = coding.forEach( (item) => {
-//     //console.log(item);
-//     return item
-// } )
-
-// console.log(values);//for each don't return 
-
-const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-const newNums = myNums.filter( (num) => {
-    return num > 4
-} )
-
-// const newNums = []
-
-// myNums.forEach( (num) => {
-//     if (num > 4) {
-//         newNums.push(num)
-//     }
-// } )
-
-console.log(newNums);
-
 
 // const books = [
 //     { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
